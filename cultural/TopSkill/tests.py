1cf15faf9ed78d97be17334429bc28dfbe516dbb
gha.ahmad205@gmail.com <gha.ahmad205@gmail.com>
2022-12-03 15:17:37 +0330

    add first test

--- cultural/TopSkill/tests.py
+++ cultural/TopSkill/tests.py
@@ -2,27 +2,27 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 
-class UsersManagersTests(TestCase):
-
-    def test_create_user(self):
-        User = get_user_model()
-        user = User.objects.create_user(email='normal@user.com', password='foo')
-        self.assertEqual(user.email, 'normal@user.com')
-        self.assertTrue(user.is_active)
-        self.assertFalse(user.is_staff)
-        self.assertFalse(user.is_superuser)
-        try:
-            # username is None for the AbstractUser option
-            # username does not exist for the AbstractBaseUser option
-            self.assertIsNone(user.username)
-        except AttributeError:
-            pass
-        with self.assertRaises(TypeError):
-            User.objects.create_user()
-        with self.assertRaises(TypeError):
-            User.objects.create_user(email='')
-        with self.assertRaises(ValueError):
-            User.objects.create_user(email='', password="foo")
+# class UsersManagersTests(TestCase):
+#
+#     def test_create_user(self):
+#         User = get_user_model()
+#         user = User.objects.create_user(email='normal@user.com', password='foo')
+#         self.assertEqual(user.email, 'normal@user.com')
+#         self.assertTrue(user.is_active)
+#         self.assertFalse(user.is_staff)
+#         self.assertFalse(user.is_superuser)
+#         try:
+#             # username is None for the AbstractUser option
+#             # username does not exist for the AbstractBaseUser option
+#             self.assertIsNone(user.username)
+#         except AttributeError:
+#             pass
+#         with self.assertRaises(TypeError):
+#             User.objects.create_user()
+#         with self.assertRaises(TypeError):
+#             User.objects.create_user(email='')
+#         with self.assertRaises(ValueError):
+#             User.objects.create_user(email='', password="foo")
 
     def test_create_superuser(self):
         User = get_user_model()
@@ -40,3 +40,25 @@ class UsersManagersTests(TestCase):
         with self.assertRaises(ValueError):
             User.objects.create_superuser(
                 email='super@user.com', password='foo', is_superuser=False)
+
+class TopSkillTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        print("setUpTestData: Run once to set up non-modified data for all class methods.")
+        pass
+
+    def setUp(self):
+        print("setUp: Run once for every test method to setup clean data.")
+        pass
+
+    def test_false_is_false(self):
+        print("Method: test_false_is_false.")
+        self.assertFalse(False)
+
+    def test_false_is_true(self):
+        print("Method: test_false_is_true.")
+        self.assertTrue(True)
+
+    def test_one_plus_one_equals_two(self):
+        print("Method: test_one_plus_one_equals_two.")
+        self.assertEqual(1 + 1, 2)
